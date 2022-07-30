a22b0189683e8be801d01a1076923aea8570201d
Anna <107642838+Kashitsina@users.noreply.github.com>
2022-07-30 21:04:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+void Zadacha67()
+{
+  // Напишите программу ,которая будет принимать на вход число и возвращать сумму его цифр
+  Console.Write("Введите числа");
+  int n = Convert.ToInt32(Console.ReadLine());
+  int summa = FindSum(n) ;
+  int FindSum(int num)
+  {
+    if (num / 10 > 0) return FindSum(num / 10)+ num%10;
+
+    return num;
+  }
+  Console.WriteLine(summa);
+
+
+} 
+
+
+Zadacha67();
